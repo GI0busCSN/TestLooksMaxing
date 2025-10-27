30dff97e771583894a5ed2afaa8250a458ba27a1
Hlib Boiko <61419088+GI0busCSN@users.noreply.github.com>
2025-10-27 12:29:05 +0100

    Add files via upload

---
+++ assets/FrontFaceNative-legacy-BGO2tk2o.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-BUR-15FX.js"],function(e,t){"use strict";var n,r,u,s,c,f;return{setters:[e=>{n=e._,r=e.r,u=e.f,s=e.o,c=e.w,f=e.e}],execute:function(){e("default",n({},[["render",function(e,t){const n=r("MaxText");return s(),u(n,null,{default:c(()=>[...t[0]||(t[0]=[f("NATIVE",-1)])]),_:1})}]]))}}});
